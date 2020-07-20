2b06c39d68a371399a8ba067a1676c3873f008c3
Ivan Barnashev <ivan.barnashev@gmail.com>
2020-07-20 09:24:40 +0300

    Commit test

---
+++ Codewars Multiply/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Codewars_Multiply
+{
+    class Program
+    {
+        public static int Multiply (int a, int b) {
+            return a * b;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.Write("Enter the first number to multiply: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number to multiply: ");
+            b = int.Parse(Console.ReadLine());
+
+            int result = Multiply(a, b);
+            Console.WriteLine($"\nThe result of multiplying a by b: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
